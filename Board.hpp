cf88684ff94fe59e4b57e293a830da8a8bc9f7a6
Orya-s <orya.sp@gmail.com>
2021-03-25 17:42:35 +0200

    git init

---
+++ Board.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include "Direction.hpp"
+
+
+namespace ariel {
+	class Board{
+        private:
+            uint rows, cols;
+
+
+        public:
+            Board() {}
+            ~Board () {}
+            void static post(int row, int col, Direction direction, std::string&& message);
+            std::string static read(int row, int col, Direction direction, int length);
+            void show();
+    };
+};
